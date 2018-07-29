3b13df854a5cb5819281370013a2a0d64b33104d
syukai <d_hirayama@rapid.ocn.ne.jp>
2018-07-29 09:23:46 +0900

    3-5-3

--- 3/softmax.py
+++ 3/softmax.py
@@ -1,4 +1,8 @@
-# ソフトマックス
+# ソフトマックス関数
+# ・inputの大小関係とoutputの大小関係は必ず同じになる
+# ・配列でない値を渡すと戻り値は必ず1.0になる
+# →分類に使用する際は最大のものを選ぶだけなので省略される
+# ・合計は必ず1.0になるので、確率として扱うことができる
 import numpy as np
 
 def softmax(a):
@@ -6,4 +10,4 @@ def softmax(a):
     exp_a = np.exp(a-c) #オーバーフロー対策
     sum_exp_a = np.sum(exp_a)
     return exp_a / sum_exp_a
-    
+
