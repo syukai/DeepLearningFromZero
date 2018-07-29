2ddec8037784750fc1542d119a1d55c7380236ae
syukai <d_hirayama@rapid.ocn.ne.jp>
2018-07-30 06:17:51 +0900

    3-6-2 normalizeをTrueに変更

--- 3/3-6-2.py
+++ 3/3-6-2.py
@@ -9,7 +9,7 @@ from softmax import softmax
 import pickle
 
 def get_data():
-    (x_train, t_train), (x_test, t_test) =load_mnist(flatten=True, normalize=False)
+    (x_train, t_train), (x_test, t_test) =load_mnist(flatten=True, normalize=True, one_hot_label=False)
     return x_test, t_test
 
 def init_network():
