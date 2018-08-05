1004c913702793064d973c5048169125ff02d0e8
syukai <d_hirayama@rapid.ocn.ne.jp>
2018-08-06 06:24:57 +0900

    5-4-1

---
+++ 5/MulLayer.py
@@ -0,0 +1,21 @@
+class MulLayer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+    
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+        out = x * y
+
+        return out
+    
+    """
+    dout-微分
+    """
+    def backward(self, dout):
+        dx = dout * self.y # xとyをひっくり返す
+        dy = dout * self.x
+
+        return dx, dy
+    
