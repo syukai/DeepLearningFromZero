57465b66435635ed6f690531b782e1d892e75261
syukai <d_hirayama@rapid.ocn.ne.jp>
2018-08-18 15:06:01 +0900

    7-4

---
+++ 7/Convolution.py
@@ -0,0 +1,47 @@
+import sys,os
+sys.path.append(os.pardir)
+import numpy as np
+from common.util import im2col, col2im
+
+# Convolution
+class Convolution:
+    def __init__(self, W, b, stride=1, pad=0):
+        self.W = W
+        self.b = b
+        self.stride = stride
+        self.pad = pad
+    
+        self.col_T = None
+        self.col_W = None
+        self.x_shape = None
+
+    def forward(self, x):
+        FN, FC, FH, FW = self.W.shape
+        N, C, H, W = x.shape
+        out_h = int(1 + (H + 2*self.pad - FH) / self.stride)
+        out_w = int(1 + (W + 2*self.pad - FW) / self.stride)
+
+        col = im2col(x, FH, FW, self.stride, self.pad)
+        col_W = self.W.reshape(FN, -1).T
+        out = np.dot(col, col_W) + self.b
+
+        out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
+
+        self.col_W = col_W
+        self.col_T = col.T
+        self.x_shape = x.shape
+
+        return out
+
+    def backward(self, dout):
+        FN, C, FH, FW = self.W.shape
+        dout = dout.transpose(0,2,3,1).reshape(-1, FN)
+
+        self.db = np.sum(dout, axis=0)
+        self.dW = np.dot(self.col_T, dout)
+        self.dW = self.dW.transpose(1, 0).reshape(FN, C, FH, FW)
+
+        dcol = np.dot(dout, self.col_W.T)
+        dx = col2im(dcol, self.x_shape, FH, FW, self.stride, self.pad)
+
+        return dx
